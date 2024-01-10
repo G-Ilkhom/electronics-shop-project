bb9cb028a7364ed1333e039778a1fd9b78bfc7ee
ILKHOM <gaffarovilkhom@inbox.ru>
2024-01-10 22:18:23 +0300

    homework-3

--- tests/test_item.py
+++ tests/test_item.py
@@ -14,6 +14,14 @@ def test_item_init(test):
     assert test.quantity == 1
 
 
+def test_repr(test):
+    assert repr(test) == "Item('Смартфон', 100, 1)"
+
+
+def test_str(test):
+    assert str(test) == 'Смартфон'
+
+
 def test_calculate_total_price(test):
     assert test.calculate_total_price() == 100
 
@@ -22,6 +30,7 @@ def test_apply_discount(test):
     Item.pay_rate = 0.8
     test.apply_discount()
     assert test.price == 80
+
 
 def test_name_setter(test):
     test.name = 'Телефон'
@@ -33,6 +42,7 @@ def test_name_setter(test):
 def test_instantiate_from_csv(test):
     test.instantiate_from_csv()
     assert test.all[0].name == 'Смартфон'
+
 
 def test_string_to_number(test):
     assert type(test.string_to_number('3')) == int
